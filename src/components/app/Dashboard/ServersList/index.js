6e7d82a8499287e8d71b5c1b9d6e2175f0f682e9
Alexandre Silva <xande1231231@hotmail.com>
2020-07-28 12:00:07 -0300

    Fix layout

--- src/components/app/Dashboard/ServersList/index.js
+++ src/components/app/Dashboard/ServersList/index.js
@@ -11,10 +11,12 @@ class Home extends React.Component {
   }
   render(){
     return <div className="serverList">
+      <div className="background">
         <ServerButton isHome serverName="Ínicio" notification="20"></ServerButton>
         <ServerButton serverName="Code Building" notification="10"></ServerButton>
         <ServerButton serverName="NekoApp" notification="0"></ServerButton>
         <ServerButton serverName="Kaway Servers" notification="120"></ServerButton>
+      </div>
     </div>
   };
 }
